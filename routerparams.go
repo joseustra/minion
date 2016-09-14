912a8a4cd3cb80199a0f04d1e9bddafc9c28a028
Jose Carlos Ustra Junior <dev@ustrajunior.com>
2016-09-14 09:37:16 -0300

    Get URL query params

--- routerparams.go
+++ routerparams.go
@@ -10,6 +10,15 @@ func (c *Context) ByGet(name string) string {
 	return chi.URLParam(c.Req, name)
 }
 
+func (c *Context) ByQuery(name string) string {
+	values := c.Req.URL.Query()
+	if len(values) == 0 {
+		return ""
+	}
+
+	return values[name][0]
+}
+
 func (c *Context) ByPost(name string) string {
 	return c.Req.FormValue(name)
 }
