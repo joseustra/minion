90561d431f7a4588850ddce7661b8f47466c10a8
Jose Carlos Ustra Junior <dev@ustrajunior.com>
2016-11-07 14:22:50 -0300

    Fix jwt * option

--- jwt.go
+++ jwt.go
@@ -16,40 +16,37 @@ func CreateJWTToken(claims map[string]interface{}) (string, error) {
 // Authenticator ...
 func (c *Context) Authenticator(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if c.Engine.options.UnauthenticatedRoutes[0] != "*" {
-			unauthenticated := false
+		unauthenticated := false
 
-			for _, path := range c.Engine.options.UnauthenticatedRoutes {
-				if req.URL.Path == path {
-					unauthenticated = true
-				}
+		for _, path := range c.Engine.options.UnauthenticatedRoutes {
+			if path == "*" || req.URL.Path == path {
+				unauthenticated = true
+			}
+		}
+
+		if !unauthenticated {
+			errResp := struct {
+				Code int    `json:"status"`
+				Msg  string `json:"message"`
+			}{
+				http.StatusUnauthorized,
+				http.StatusText(http.StatusUnauthorized),
 			}
 
-			if !unauthenticated {
-				errResp := struct {
-					Code int    `json:"status"`
-					Msg  string `json:"message"`
-				}{
-					http.StatusUnauthorized,
-					http.StatusText(http.StatusUnauthorized),
-				}
+			ctx := req.Context()
 
-				ctx := req.Context()
-
-				if jwtErr, ok := ctx.Value("jwt.err").(error); ok {
-					if jwtErr != nil {
-						c.render.JSON(rw, http.StatusUnauthorized, errResp)
-						return
-					}
-				}
-
-				jwtToken, ok := ctx.Value("jwt").(*jwt.Token)
-				if !ok || jwtToken == nil || !jwtToken.Valid {
+			if jwtErr, ok := ctx.Value("jwt.err").(error); ok {
+				if jwtErr != nil {
 					c.render.JSON(rw, http.StatusUnauthorized, errResp)
 					return
 				}
 			}
 
+			jwtToken, ok := ctx.Value("jwt").(*jwt.Token)
+			if !ok || jwtToken == nil || !jwtToken.Valid {
+				c.render.JSON(rw, http.StatusUnauthorized, errResp)
+				return
+			}
 		}
 
 		next.ServeHTTP(rw, req)
