803e6b29ba64bee2d7cad382a0d1f2bf441f7dc7
Jose Carlos Ustra Junior <josecarlos@ustrajunior.com>
2017-09-17 13:44:49 -0300

    Add html return and headers

--- context.go
+++ context.go
@@ -26,10 +26,29 @@ func (app *App) createContext(rw http.ResponseWriter, req *http.Request, handler
 	return ctx
 }
 
+// SetHeader sets a response header.
+func (c *Context) SetHeader(key, value string) {
+	c.writer.Header().Set(key, value)
+}
+
 func (c *Context) JSON(status int, v interface{}) {
 	c.render.JSON(c.writer, status, v)
 }
 
 func (c *Context) Text(status int, v string) {
 	c.render.Text(c.writer, status, v)
+}
+
+func (c *Context) HTML(status int, tmpl string, v interface{}) {
+	c.render.HTML(c.writer, status, tmpl, v)
+}
+
+// Redirect returns a HTTP redirect to the specific location. default for 302
+func (c *Context) Redirect(status int, location string) {
+	c.SetHeader("Location", location)
+	if status > 0 {
+		http.Redirect(c.writer, c.Req, location, status)
+	} else {
+		http.Redirect(c.writer, c.Req, location, http.StatusTemporaryRedirect)
+	}
 }
